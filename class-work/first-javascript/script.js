7507a9de080d322bdfb293651fa3a6d8e1de9cd6
Allison Supron <supron123@allison-s-macbook-pro-3.local>
2016-03-27 23:25:59 -0400

    homework

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,27 @@
+alert('this is an alert. this is annoying');
+
+var num1 = 10 * 10;
+var num2 = 100 / 10;
+var num3 = 50 - 17 ;
+var myName = 'Allison Supron'
+
+document.write(num1);
+document.write(num2);
+document.write(num3);
+document.write(myName);
+
+document.write(num1, num2, num3, myName)
+
+function hello () {
+		var greeting = 'hello!';
+
+		greeting = 'hello, world!';
+
+		alert('greeting');
+	}
+
+hello()
+
+document.getElementById('title').style.color='red';
+
+document.getElementById('text-chunk').style.backgroundColor='blue';
